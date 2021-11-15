571886e24ee5e10c06e8dcb46e4612e23fb2d30f
flnandes <flservice2000@gmail.com>
2021-11-14 23:46:18 -0300

    crud mvc php

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'proprietary',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '5e5056a22cd195981d507ee97adacd9f38ca2a36',
+        'name' => 'cursomvc/udemy',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'cursomvc/udemy' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'proprietary',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '5e5056a22cd195981d507ee97adacd9f38ca2a36',
+            'dev_requirement' => false,
+        ),
+    ),
+);
